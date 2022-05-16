65ee244876ac85ad06493744eb366e0ec68d9d98
Jeff Sember <jpsember@gmail.com>
2022-05-15 18:19:15 -0700

    Issue #17: removing unused data class; adding augmentation parameter to compile images config

--- src/main/java/ml/feed/FeedOper.java
+++ src/main/java/ml/feed/FeedOper.java
@@ -2,8 +2,6 @@ package ml.feed;
 
 import static js.base.Tools.*;
 
-import java.util.List;
-
 import gen.FeedConfig;
 import js.app.AppOper;
 
@@ -22,11 +20,6 @@ public class FeedOper extends AppOper {
 
   @Override
   public void perform() {
-    if (alert("temporary")) {
-      exp();
-      return;
-    }
-
     FeedAlg alg = null;
     String algName = "ml.feed.Alg" + config().alg();
     Class klass = null;
@@ -49,54 +42,6 @@ public class FeedOper extends AppOper {
   @Override
   public FeedConfig config() {
     return super.config();
-  }
-
-  private void exp() {
-
-    List<Integer> vals = arrayList();
-    int targetMax = 10;
-
-    int capacity = 8;
-    float power = 0.5f;
-
-    for (int j = 100; j < 1000; j += 10) {
-      vals.add(j);
-
-      if (vals.size() > capacity) {
-        // Throw out value whose neighbors have fractions closest together
-        double[] frac = new double[vals.size()];
-        int i = INIT_INDEX;
-        for (int target : vals) {
-          i++;
-          frac[i] = fraction(target, targetMax, power);
-        }
-        double minDiff = 0;
-        int minIndex = -1;
-        for (int q = 1; q < vals.size() - 1; q++) {
-          double diff = frac[q + 1] - frac[q - 1];
-          if (minIndex < 0 || minDiff > diff) {
-            minIndex = q;
-            minDiff = diff;
-          }
-        }
-        vals.remove(minIndex);
-      }
-      pr("values:", vals);
-    }
-  }
-
-  /**
-   * Calculate epoch for a particular fraction
-   */
-  /* private */ static int epoch(double fraction, double power, int epochMax) {
-    return (int) Math.round(epochMax * Math.pow(fraction, power));
-  }
-
-  /**
-   * Calculate fraction for a particular epoch
-   */
-  private static double fraction(int epoch, int maxEpoch, double power) {
-    return Math.pow(epoch / (double) maxEpoch, 1 / power);
   }
 
 }
