75a88ce4298205bc07beaa619f897fc6b9e7255f
Jeff Sember <jpsember@gmail.com>
2022-05-17 22:38:09 -0700

    Issue #20: moving code around, merged two methods

--- src/main/java/ml/yolo/YoloModelWrapper.java
+++ src/main/java/ml/yolo/YoloModelWrapper.java
@@ -121,8 +121,161 @@ public final class YoloModelWrapper extends ModelWrapper<Yolo> {
 
   @Override
   public void parseInferenceResult(byte[] modelOutput, Script.Builder script) {
-    float[] imageLabelData = DataUtil.bytesToFloatsLittleEndian(modelOutput);
-    List<ScriptElement> boxList = readImageResult(0.8f, imageLabelData);
+    Yolo yolo = modelConfig();
+    float[] f = DataUtil.bytesToFloatsLittleEndian(modelOutput);
+    float confidenceThreshold = 0.8f; // TODO: make this a parameter
+
+    todo(
+        "Perhaps the x,y coordinates should be the location of the center of the anchor box, relative to the center of the grid cell");
+    log("Constructing YOLO result for image");
+    log("...confidence threshold %", pct(confidenceThreshold));
+
+    if (!alert("this is probably redundant")) {
+      int expectedDataLength = mGridSize.product() * YoloUtil.valuesPerBlock(yolo);
+      if (f.length != expectedDataLength) {
+        throw die("length of image data:", f.length, "!= expected value:", expectedDataLength, INDENT, yolo);
+      }
+    }
+
+    List<ScriptElement> boxList = arrayList();
+
+    int anchorBoxCount = YoloUtil.anchorBoxCount(yolo);
+    int fieldsPerBox = YoloUtil.valuesPerAnchorBox(yolo);
+
+    final float logitMinForResult = NetworkUtil.logit(confidenceThreshold);
+
+    int categoryCount = yolo.categoryCount();
+
+    List<CategoryConfidence> categoryConfidences = arrayList();
+    StringBuilder sbLine = null;
+    StringBuilder sbGrid = null;
+    JSMap gridMap = null;
+    if (verbose()) {
+      sbLine = new StringBuilder();
+      sbGrid = new StringBuilder();
+      gridMap = map();
+    }
+
+    float highestObjectnessLogitSeen = 0f;
+    int fieldSetIndex = 0;
+    for (int cellY = 0; cellY < mGridSize.y; cellY++) {
+      if (verbose()) {
+        sbGrid.setLength(0);
+        sbGrid.append("[");
+      }
+
+      for (int cellX = 0; cellX < mGridSize.x; cellX++) {
+
+        char cellString = 0;
+        for (int anchorBox = 0; anchorBox < anchorBoxCount; anchorBox++, fieldSetIndex += fieldsPerBox) {
+
+          float objectnessLogit = f[fieldSetIndex + F_CONFIDENCE];
+          // Note, this check will cause us to skip a lot of computation, which
+          // suggests we probably don't want to embed the sigmoid/exp postprocessing steps into the model
+          if (objectnessLogit < logitMinForResult)
+            continue;
+
+          highestObjectnessLogitSeen = Math.max(highestObjectnessLogitSeen, objectnessLogit);
+          float objectnessConfidence = NetworkUtil.sigmoid(objectnessLogit);
+
+          if (verbose()) {
+            sbLine.setLength(0);
+            sbLine.append(String.format("y%2d  x%2d  ", cellY, cellX));
+            if (anchorBoxCount > 1)
+              sbLine.append(String.format("a%d  ", anchorBox));
+            sbLine.append(String.format("%5.1f  ", pct(objectnessConfidence)));
+          }
+
+          IPoint anchorBoxPixels = yolo.anchorBoxesPixels().get(anchorBox);
+          float anchorBoxWidth = anchorBoxPixels.x;
+          float anchorBoxHeight = anchorBoxPixels.y;
+
+          CategoryConfidence bestCategory = CategoryConfidence.DEFAULT_INSTANCE;
+          if (categoryCount > 1) {
+            int k = fieldSetIndex + F_CLASS_PROBABILITIES;
+            categoryConfidences.clear();
+            for (int i = 0; i < categoryCount; i++)
+              categoryConfidences
+                  .add(CategoryConfidence.newBuilder().category(i).confidenceLogit(f[k + i]).build());
+            Collections.sort(categoryConfidences,
+                (a, b) -> -Float.compare(a.confidenceLogit(), b.confidenceLogit()));
+            bestCategory = categoryConfidences.get(0);
+            if (verbose()) {
+              CategoryConfidence second = categoryConfidences.get(1);
+              sbLine.append(toString(bestCategory));
+              sbLine.append(toString(second));
+              sbLine.append(" ");
+            }
+            if (cellString == 0)
+              cellString = (char) ('0' + bestCategory.category());
+          }
+
+          int k = fieldSetIndex + F_BOX_XYWH;
+          if (verbose()) {
+            sbLine
+                .append(String.format("logt[%5.1f %5.1f %5.1f %5.1f]  ", f[k], f[k + 1], f[k + 2], f[k + 3]));
+          }
+
+          // Note that the x,y (centerpoints) are relative to the cell,
+          // while the width and height are relative to the anchor box size
+
+          float bx = NetworkUtil.sigmoid(f[k + 0]);
+          float by = NetworkUtil.sigmoid(f[k + 1]);
+          float ws = NetworkUtil.exp(f[k + 2]);
+          float hs = NetworkUtil.exp(f[k + 3]);
+
+          todo("but can we precalculate the training labels to save some calc?");
+
+          if (verbose()) {
+            sbLine.append(String.format("sg/ex[%4.2f %4.2f %4.2f %4.2f]  ", bx, by, ws, hs));
+          }
+
+          float bw = anchorBoxWidth * ws;
+          float bh = anchorBoxHeight * hs;
+          float midpointX = (bx + cellX) * mGridToImageScale.x;
+          float midpointY = (by + cellY) * mGridToImageScale.y;
+
+          IRect boxRect = new FRect(midpointX - bw / 2, midpointY - bh / 2, bw, bh).toIRect();
+
+          // I think we want to use the 'objectness' confidence, without incorporating the best category's confidence in any way
+          ElementProperties.Builder prop = ElementProperties.newBuilder();
+          prop.category(bestCategory.category());
+          prop.confidence(MyMath.parameterToPercentage(objectnessConfidence));
+          // todo: add support anchor box property? 
+          // prop.anchor(anchorBox)
+          RectElement boxObj = new RectElement(prop, boxRect);
+
+          if (verbose()) {
+            sbLine.append(boxObj.bounds());
+            log(sbLine.toString());
+          }
+          boxList.add(boxObj);
+        }
+
+        if (verbose()) {
+          if (cellString == 0)
+            cellString = ':';
+          sbGrid.append(' ');
+          sbGrid.append(cellString);
+        }
+      }
+      if (verbose()) {
+        sbGrid.append("]");
+        gridMap.put(String.format("y %2d", cellY), sbGrid.toString());
+      }
+    }
+
+    if (verbose()) {
+      pr();
+      pr(gridMap);
+      pr();
+      pr("Valid anchor boxes:", boxList.size());
+      todo("do we always want to perform sigmoid here, i.e., is it in input or an output?");
+      pr("Highest conf:", pct(NetworkUtil.sigmoid(highestObjectnessLogitSeen)));
+      if (boxList.isEmpty())
+        pr("*** No boxes detected");
+    }
+
     if (mParserConfig.maxIOverU() > 0)
       boxList = YoloUtil.performNonMaximumSuppression(boxList, mParserConfig.maxIOverU());
     script.items(boxList);
@@ -436,165 +589,6 @@ public final class YoloModelWrapper extends ModelWrapper<Yolo> {
   private int mFieldsPerGridCell;
   private int mFieldsPerImage;
   private float[] mOutputLayer;
-
-  private List<ScriptElement> readImageResult(float confidenceThreshold, float[] imageData) {
-    todo("rename this method");
-    todo(
-        "Perhaps the x,y coordinates should be the location of the center of the anchor box, relative to the center of the grid cell");
-    log("Constructing YOLO result for image");
-    log("...confidence threshold %", pct(confidenceThreshold));
-
-    Yolo yolo = modelConfig();
-
-    int expectedDataLength = mGridSize.product() * YoloUtil.valuesPerBlock(yolo);
-    if (imageData.length != expectedDataLength) {
-      throw die("length of image data:", imageData.length, "!= expected value:", expectedDataLength, INDENT,
-          yolo);
-    }
-
-    List<ScriptElement> boxList = arrayList();
-
-    float[] f = imageData;
-
-    int anchorBoxCount = YoloUtil.anchorBoxCount(yolo);
-    int fieldsPerBox = YoloUtil.valuesPerAnchorBox(yolo);
-
-    final float logitMinForResult = NetworkUtil.logit(confidenceThreshold);
-
-    int categoryCount = yolo.categoryCount();
-
-    List<CategoryConfidence> categoryConfidences = arrayList();
-    StringBuilder sbLine = null;
-    StringBuilder sbGrid = null;
-    JSMap gridMap = null;
-    if (verbose()) {
-      sbLine = new StringBuilder();
-      sbGrid = new StringBuilder();
-      gridMap = map();
-    }
-
-    float highestObjectnessLogitSeen = 0f;
-    int fieldSetIndex = 0;
-    for (int cellY = 0; cellY < mGridSize.y; cellY++) {
-      if (verbose()) {
-        sbGrid.setLength(0);
-        sbGrid.append("[");
-      }
-
-      for (int cellX = 0; cellX < mGridSize.x; cellX++) {
-
-        char cellString = 0;
-        for (int anchorBox = 0; anchorBox < anchorBoxCount; anchorBox++, fieldSetIndex += fieldsPerBox) {
-
-          float objectnessLogit = f[fieldSetIndex + F_CONFIDENCE];
-          // Note, this check will cause us to skip a lot of computation, which
-          // suggests we probably don't want to embed the sigmoid/exp postprocessing steps into the model
-          if (objectnessLogit < logitMinForResult)
-            continue;
-
-          highestObjectnessLogitSeen = Math.max(highestObjectnessLogitSeen, objectnessLogit);
-          float objectnessConfidence = NetworkUtil.sigmoid(objectnessLogit);
-
-          if (verbose()) {
-            sbLine.setLength(0);
-            sbLine.append(String.format("y%2d  x%2d  ", cellY, cellX));
-            if (anchorBoxCount > 1)
-              sbLine.append(String.format("a%d  ", anchorBox));
-            sbLine.append(String.format("%5.1f  ", pct(objectnessConfidence)));
-          }
-
-          IPoint anchorBoxPixels = yolo.anchorBoxesPixels().get(anchorBox);
-          float anchorBoxWidth = anchorBoxPixels.x;
-          float anchorBoxHeight = anchorBoxPixels.y;
-
-          CategoryConfidence bestCategory = CategoryConfidence.DEFAULT_INSTANCE;
-          if (categoryCount > 1) {
-            int k = fieldSetIndex + F_CLASS_PROBABILITIES;
-            categoryConfidences.clear();
-            for (int i = 0; i < categoryCount; i++)
-              categoryConfidences
-                  .add(CategoryConfidence.newBuilder().category(i).confidenceLogit(f[k + i]).build());
-            Collections.sort(categoryConfidences,
-                (a, b) -> -Float.compare(a.confidenceLogit(), b.confidenceLogit()));
-            bestCategory = categoryConfidences.get(0);
-            if (verbose()) {
-              CategoryConfidence second = categoryConfidences.get(1);
-              sbLine.append(toString(bestCategory));
-              sbLine.append(toString(second));
-              sbLine.append(" ");
-            }
-            if (cellString == 0)
-              cellString = (char) ('0' + bestCategory.category());
-          }
-
-          int k = fieldSetIndex + F_BOX_XYWH;
-          if (verbose()) {
-            sbLine
-                .append(String.format("logt[%5.1f %5.1f %5.1f %5.1f]  ", f[k], f[k + 1], f[k + 2], f[k + 3]));
-          }
-
-          // Note that the x,y (centerpoints) are relative to the cell,
-          // while the width and height are relative to the anchor box size
-
-          float bx = NetworkUtil.sigmoid(f[k + 0]);
-          float by = NetworkUtil.sigmoid(f[k + 1]);
-          float ws = NetworkUtil.exp(f[k + 2]);
-          float hs = NetworkUtil.exp(f[k + 3]);
-
-          todo("but can we precalculate the training labels to save some calc?");
-
-          if (verbose()) {
-            sbLine.append(String.format("sg/ex[%4.2f %4.2f %4.2f %4.2f]  ", bx, by, ws, hs));
-          }
-
-          float bw = anchorBoxWidth * ws;
-          float bh = anchorBoxHeight * hs;
-          float midpointX = (bx + cellX) * mGridToImageScale.x;
-          float midpointY = (by + cellY) * mGridToImageScale.y;
-
-          IRect boxRect = new FRect(midpointX - bw / 2, midpointY - bh / 2, bw, bh).toIRect();
-
-          // I think we want to use the 'objectness' confidence, without incorporating the best category's confidence in any way
-          ElementProperties.Builder prop = ElementProperties.newBuilder();
-          prop.category(bestCategory.category());
-          prop.confidence(MyMath.parameterToPercentage(objectnessConfidence));
-          // todo: add support anchor box property? 
-          // prop.anchor(anchorBox)
-          RectElement boxObj = new RectElement(prop, boxRect);
-
-          if (verbose()) {
-            sbLine.append(boxObj.bounds());
-            log(sbLine.toString());
-          }
-          boxList.add(boxObj);
-        }
-
-        if (verbose()) {
-          if (cellString == 0)
-            cellString = ':';
-          sbGrid.append(' ');
-          sbGrid.append(cellString);
-        }
-      }
-      if (verbose()) {
-        sbGrid.append("]");
-        gridMap.put(String.format("y %2d", cellY), sbGrid.toString());
-      }
-    }
-
-    if (verbose()) {
-      pr();
-      pr(gridMap);
-      pr();
-      pr("Valid anchor boxes:", boxList.size());
-      todo("do we always want to perform sigmoid here, i.e., is it in input or an output?");
-      pr("Highest conf:", pct(NetworkUtil.sigmoid(highestObjectnessLogitSeen)));
-      if (boxList.isEmpty())
-        pr("*** No boxes detected");
-    }
-
-    return boxList;
-  }
 
   private static float pct(float confidence) {
     return confidence * 100;
