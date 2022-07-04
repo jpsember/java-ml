1ffc35e1ba6d421eaac23a6c209f5858b76cb7e1
Jeff Sember <jpsember@gmail.com>
2022-07-03 17:49:01 -0700

    Issue #42

--- example_yolo/yolo_loss.py
+++ example_yolo/yolo_loss.py
@@ -18,7 +18,7 @@ class YoloLoss(nn.Module):
     self.grid_size = grid_size(yolo)
     self.grid_cell_total = self.grid_size.product()
     self.log_counter = 0
-
+    self.logged_tensor_count
 
   def forward(self, current, target):
 
@@ -175,6 +175,9 @@ class YoloLoss(nn.Module):
       return
     if name.startswith("."):
       return
+    if self.logged_tensor_count >= 30:
+      return
+    self.logged_tensor_count += 1
 
     # If tensor not provided, assume name refers to a local variable in the caller's scope
     #
